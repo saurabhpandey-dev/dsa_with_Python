a3a63e7b7a7e8449bdd9d8ec85fa40582aea90fb
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-13 23:35:33 +0530

    Answer 1
    solve Qustion 1

---
+++ Ans_1_total_number_of_characters.py
@@ -0,0 +1,6 @@
+## QUS 1. Write a program to find and display the total number of characters
+## present in a given string.
+
+str = 'Hello world'
+total_char = len(str)
+print(f'total number of Sting :-{total_char}.') # with White Space
