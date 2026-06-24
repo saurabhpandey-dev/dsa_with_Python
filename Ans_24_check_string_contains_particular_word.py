00e74e0e113253366ca5e9d5b3ea15804bfeeb7f
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-24 13:25:04 +0530

    Answer_24
    check whether a string contains a particular word or substring.

---
+++ Ans_24_check_string_contains_particular_word.py
@@ -0,0 +1,11 @@
+##Ans_ 24. Write a program to check whether a string contains a particular word or
+##substring.
+
+
+str1 = 'ich bin aus mirzapur'
+contains = 'bin'
+
+if contains in str1:
+    print('yes')
+else:
+    print("no")
