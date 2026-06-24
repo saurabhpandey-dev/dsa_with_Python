7c2529543f0175db3044eba916b430f5c9b2353d
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-24 13:24:36 +0530

    Answer_23
    check whether a string ends with a given suffix

---
+++ Ans_23_string_ends_with_given_suffix.py
@@ -0,0 +1,8 @@
+##Ans_23. Write a program to check whether a string ends with a given suffix.
+
+str1 = 'saurabh'
+
+if str1.endswith('bh'):
+    print('yes it have it!')
+else:
+    print("not have given suffix")
