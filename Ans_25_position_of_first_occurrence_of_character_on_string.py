fccd2ffc47873712ba670e1d6463a643c636f226
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-24 13:25:34 +0530

    Answer_25
    find the position of the first occurrence of a character in a string.

---
+++ Ans_25_position_of_first_occurrence_of_character_on_string.py
@@ -0,0 +1,13 @@
+##QUS_25. Write a program to find the position of the first occurrence of a character in a
+##string.
+
+
+string = 'saurabh'
+def first(s):
+       print(string)
+       print(f'"{s}" index is {string.index(s)}')
+       print()
+
+
+first('r')
+first('b')
