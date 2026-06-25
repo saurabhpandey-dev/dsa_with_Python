c0cb8e6f2ffa08569dda347efe9e8e28b385f991
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-25 21:07:45 +0530

    Answer_28

---
+++ Ans_28_split_a_sentence_into_individual_words.py
@@ -0,0 +1,12 @@
+##QUS_28. Write a program to split a sentence into individual words and
+##display them.
+
+
+string = 'ich komme aus mirzapur'
+lst = []
+
+for s in string.split(' '):
+    lst.append(s)
+
+print(lst)
+    
