663a70ec96ccb20008b93d5503188e05f57614a4
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-25 21:07:14 +0530

    Answer_26

---
+++ Ans_26_position_of_last_occurrence_of_string.py
@@ -0,0 +1,17 @@
+##QUS_26. Write a program to find the position of the last occurrence of a
+##character in a string.
+
+
+string ='saurabh'
+
+def last(s):
+    print(string)
+    return string.rfind('a')
+    print()        
+
+
+
+x = input('enter the char : ')
+print(f'second index of {x} is {last(x)}')
+
+
