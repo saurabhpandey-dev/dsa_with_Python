c5555f25a2795bdc4584f16c594b67f8de388dde
Saurabh Pandey <sp8355063538@gmail.com>
2026-06-25 21:07:52 +0530

    Answer_29

---
+++ Ans_29_join_multiple_strings_stored_in_array.py
@@ -0,0 +1,14 @@
+##QUS_29. Write a program to join multiple strings stored in an array into a single
+##sentence.
+
+
+lst = ['saurabh','pandey']
+
+string = " ".join(lst)
+
+print(f'List is : {lst}')
+
+print(f'convert int string : {string}')
+
+print(f'type of : {type(string)}')
+
