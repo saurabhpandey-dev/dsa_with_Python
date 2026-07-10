2a0f12c8ccddafc2b6202c2e42f330a3d3dfdcee
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-10 22:51:14 +0530

    add get length of list
    get the length of linked list

---
+++ Link_List/Singly_link_list/get_length.py
@@ -0,0 +1,47 @@
+class node:
+    def __init__(self,data=None,next=None):
+        self.data=data
+        self.next=next
+class link:
+    def __init__(self,head=None):
+        self.head = head
+        
+    def is_empty(self):
+        return self.head is None
+
+    def insert(self,data):
+        newnode = node(data)
+        temp = self.head
+        if self.is_empty():
+            self.head = newnode
+            return
+        while(temp.next is not None):
+            temp = temp.next
+        
+        temp.next = newnode
+    
+    def traverse(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data,end='->')
+            temp = temp.next
+            
+    def length(self):
+        temp = self.head
+        l=1
+        while(temp.next is not None):
+            l+=1
+            temp = temp.next
+        return l
+    
+    
+one = link()
+one.insert(10)
+one.insert(20)
+one.insert(30)
+one.insert(40)
+one.insert(50)
+
+one.traverse()   
+print()
+print(one.length())
