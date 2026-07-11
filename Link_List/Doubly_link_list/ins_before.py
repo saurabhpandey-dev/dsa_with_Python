49ed73d29c92ceb0c52e3500d751a39ef16ccd4c
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-12 01:09:16 +0530

    add insert node before any node
    insert node before any node you want

---
+++ Link_List/Doubly_link_list/ins_before.py
@@ -0,0 +1,78 @@
+class Node:
+    def __init__(self,data=None,prev=None,next=None):
+        self.prev = prev
+        self.data = data
+        self.next = next
+        
+class Link:
+    def __init__(self,head=None):
+        self.head = head
+    
+    def is_empty(self):
+        return self.head is None
+    
+    def insert(self,data):
+        temp = self.head
+        newnode = Node(data)
+        if self.head is None:
+            self.head = newnode
+            return
+        while temp.next is not None:
+            temp=temp.next
+        newnode.prev = temp
+        temp.next = newnode
+    
+    def traverse(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data,end=' -> ')
+            temp = temp.next
+        print(None)
+    
+    def length(self):
+        temp = self.head
+        l=1
+        while(temp.next is not None):
+            l+=1
+            temp = temp.next
+        return l 
+    
+    def ins_before(self,data,before=None):
+        if self.head == None:
+            print('List is empty !!')
+            return
+        newnode = Node(data)
+        temp = self.head
+        
+        if before==None:
+            while temp.next is not None:
+                temp=temp.next
+            temp.next = newnode
+            newnode.prev=temp
+            
+        if self.head.data == before:
+            newnode.next = self.head
+            self.head.next.prev = newnode
+            self.head=newnode
+            return
+        while temp.next is not None:
+            if temp.next.data == before:
+                newnode.next = temp.next 
+                newnode.prev = temp
+                temp.next.prev = newnode
+                temp.next = newnode
+                return
+            temp=temp.next
+                   
+    
+one = Link()
+one.insert(10)
+one.insert(20)
+one.insert(30)
+one.insert(40)
+one.insert(50)            
+
+one.traverse()
+print()
+one.ins_before(25,40)
+one.traverse()
