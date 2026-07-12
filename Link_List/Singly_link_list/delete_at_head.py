74ac9cf9793db14663059277124702a8ccb9c802
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-12 23:36:25 +0530

    add del head node
    delete first node in singly link list

---
+++ Link_List/Singly_link_list/delete_at_head.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self,data=None,next=None):
+        self.data = data
+        self.next = next
+        
+class Link:
+    def __init__(self,head=None):
+        self.head = head
+    
+    def insert(self,data=0):
+        temp = self.head
+        newnode = Node(data)
+        if self.head is None:
+            self.head = newnode
+            return
+        while(temp.next is not None):
+            temp = temp.next 
+        temp.next = newnode
+    
+    def del_head(self):
+        if self.head is None:
+            print('List is empty!!')
+            return
+        temp = self.head.next
+        self.head = temp
+        
+    
+    def traverse(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data,end=' -> ')
+            temp =temp.next 
+        print(None,'\n')
+   
+    
+one = Link()
+
+one.insert(10)
+one.insert(20)
+one.insert(30) 
+one.insert(40)
+
+one.traverse()
+one.del_head()
+one.traverse()
