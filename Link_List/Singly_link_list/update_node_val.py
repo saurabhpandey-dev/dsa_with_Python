c11a81e6c3229e3af393502889aef6f81b3caccf
Saurabh Pandey <sp8355063538@gmail.com>
2026-07-13 20:11:53 +0530

    add update value
    update value of any desired node

---
+++ Link_List/Singly_link_list/update_node_val.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self,data=None,next=None):
+        self.data=data
+        self.next=next
+
+class link:
+    def __init__(self,head=None):
+        self.head = head
+    
+    def is_empty(self):
+        return self.head is None
+    
+    def insert(self,data):
+        newnode = Node(data)
+        temp = self.head
+        if self.is_empty():
+            self.head = newnode
+        else:
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = newnode
+                
+    def traverse(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data,end=" -> ")
+            temp = temp.next
+        print('None\n')
+        
+    def change(self,past,future):
+        temp = self.head
+        
+        if self.head is None:
+            print('list is empty !!')
+            return
+        
+        while(temp is not None):
+            if temp.data == past:
+                temp.data = future
+                return
+            temp=temp.next
+        print('value not found !!')
+        
+one = link()
+one.insert(1)
+one.insert(2)
+one.insert(3)
+one.insert(4)
+one.insert(5)
+
+one.traverse()
+one.change(5,3.5)
+one.traverse()
